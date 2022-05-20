99b8e93867cba5d1b76a96058d457eddaceca25b
Mohammed Musaib Ali <mdmusaibali@gmail.com>
2022-05-20 18:31:10 +0530

    First commit

---
+++ src/js/script.js
@@ -0,0 +1,18 @@
+const audio = document.getElementById("audioplayer");
+const range = document.querySelector(".range");
+const audioIcon = document.querySelector(".icon-controls");
+const controlButton = document.querySelector(".button-play-pause");
+
+controlButton.addEventListener("click", function () {
+  if (audio.paused) {
+    audio.play();
+    audioIcon.setAttribute("name", "pause-outline");
+  } else {
+    audio.pause();
+    audioIcon.setAttribute("name", "play-outline");
+  }
+});
+
+range.addEventListener("mousemove", function (e) {
+  audio.volume = e.target.value;
+});
